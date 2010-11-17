882a54e71b4ebe872f7386bbeac56c73e4b1cc36
coutant <coutant@coutant.(none)>
2010-11-17 18:38:45 +0100

    Remote control support

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
-# ~/prog/flex3.5-air2.5/bin/amxmlc -library-path+=libs/cairngorm-2.2.1.swc src/main/flex/airtv.mxml 
+~/prog/flex3.5-air2.5/bin/amxmlc -library-path+=libs/cairngorm-2.2.1.swc src/main/flex/airtv.mxml 
 # compiling Flex 3 source with Flex 4 SDK. the reason for halo.swc. You'll find it in : frameworks/themes/Halo/
-~/prog/flex4.1-air2.5/bin/amxmlc -library-path+=libs/cairngorm-2.2.1.swc -theme=halo.swc src/main/flex/airtv.mxml 
+# -theme=halo.swc
+# ~/prog/flex4.1-air2.5/bin/amxmlc -library-path+=libs/cairngorm-2.2.1.swc  -compatibility-version=3 src/main/flex/airtv.mxml 
